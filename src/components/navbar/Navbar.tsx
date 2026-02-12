87e0ae32d635ae49f9b749259227f1f037a3ebd5
Randerson Henrique <randerson.henrique4@gmail.com>
2026-02-12 15:43:23 -0300

    pagina de login

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,18 +1,20 @@
+import { Link } from "react-router-dom"
+
 function Navbar() {
     return (
         <>
-            <div className='w-full flex justify-center py-4 bg-indigo-900 text-white'>
-                
+            <div className='w-full flex justify-center py-4
+                            bg-indigo-900 text-white'>
+
                 <div className="container flex justify-between text-lg mx-8">
-                    {/* Este é o título que não estava aparecendo no seu */}
-                    <div className='font-bold uppercase'>Blog Pessoal</div>
+                    <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
 
                     <div className='flex gap-4'>
-                        <div className='hover:underline cursor-pointer'>Postagens</div>
-                        <div className='hover:underline cursor-pointer'>Temas</div>
-                        <div className='hover:underline cursor-pointer'>Cadastrar tema</div>
-                        <div className='hover:underline cursor-pointer'>Perfil</div>
-                        <div className='hover:underline cursor-pointer'>Sair</div>
+                        Postagens
+                        Temas
+                        Cadastrar tema
+                        Perfil
+                        Sair
                     </div>
                 </div>
             </div>
