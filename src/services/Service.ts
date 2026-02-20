49264ed71035d8942f867dba564a6f2d4e2181e5
Randerson Henrique <randerson.henrique4@gmail.com>
2026-02-20 12:31:04 -0300

    Criação do delete

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://blogpessoal.onrender.com'
+  baseURL: 'https://blog-pessoal-1-1pm7.onrender.com'
 })
 
 // eslint-disable-next-line @typescript-eslint/no-wrapper-object-types
@@ -21,4 +21,19 @@ export const login = async(url: string, dados: Object, setDados: Function) => {
 export const buscar = async(url: string, setDados: Function, header: Object) => {
   const resposta = await api.get(url, header)
   setDados(resposta.data)
+}
+// eslint-disable-next-line @typescript-eslint/no-wrapper-object-types
+export const cadastrar = async (url: string, dados: Object, setDados: Function, header: Object) => {
+    const resposta = await api.post(url, dados, header)
+    setDados(resposta.data)
+}
+
+// eslint-disable-next-line @typescript-eslint/no-wrapper-object-types
+export const atualizar = async (url: string, dados: Object, setDados: Function, header: Object) => {
+    const resposta = await api.put(url, dados, header)
+    setDados(resposta.data)
+}
+ // eslint-disable-next-line @typescript-eslint/no-wrapper-object-types
+ export const deletar = async (url: string, header: Object) => {
+    await api.delete(url, header)
 }
