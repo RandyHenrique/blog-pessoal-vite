2559aef696baa53972be0c41ff36cdaf8c771b12
Randerson Henrique <randerson.henrique4@gmail.com>
2026-02-27 10:13:05 -0300

    Deploy do Projeto Projeto Blog Pessoal

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,39 +1,36 @@
 /* eslint-disable @typescript-eslint/no-unsafe-function-type */
+/* eslint-disable @typescript-eslint/no-wrapper-object-types */
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://blog-pessoal-1-1pm7.onrender.com'
+  baseURL: import.meta.env.VITE_API_URL
 })
 
-// eslint-disable-next-line @typescript-eslint/no-wrapper-object-types
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
   const resposta = await api.post(url, dados)
   setDados(resposta.data)
 }
 
-// eslint-disable-next-line @typescript-eslint/no-wrapper-object-types
 export const login = async(url: string, dados: Object, setDados: Function) => {
   const resposta = await api.post(url, dados)
   setDados(resposta.data)
 }
 
-// eslint-disable-next-line @typescript-eslint/no-wrapper-object-types
 export const buscar = async(url: string, setDados: Function, header: Object) => {
   const resposta = await api.get(url, header)
   setDados(resposta.data)
 }
-// eslint-disable-next-line @typescript-eslint/no-wrapper-object-types
+
 export const cadastrar = async (url: string, dados: Object, setDados: Function, header: Object) => {
     const resposta = await api.post(url, dados, header)
     setDados(resposta.data)
 }
 
-// eslint-disable-next-line @typescript-eslint/no-wrapper-object-types
 export const atualizar = async (url: string, dados: Object, setDados: Function, header: Object) => {
     const resposta = await api.put(url, dados, header)
     setDados(resposta.data)
 }
- // eslint-disable-next-line @typescript-eslint/no-wrapper-object-types
- export const deletar = async (url: string, header: Object) => {
+
+export const deletar = async (url: string, header: Object) => {
     await api.delete(url, header)
 }
